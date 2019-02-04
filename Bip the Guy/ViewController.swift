d051c8ab62ca154c133223286588194562e9e687
Sarah Kim <sarah.mjk@hotmail.com>
2019-02-03 23:31:47 -0500

    Initial Commit

---
+++ Bip the Guy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bip the Guy
+//
+//  Created by Sarah Minji Kim on 2/3/19.
+//  Copyright © 2019 Sarah Minji Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
